c1e5303abf1330747b0d579714f7f2553c427eab
Vert <79771507+grep-fruit@users.noreply.github.com>
2022-07-12 16:29:37 -0400

    Add files via upload

---
+++ js/clicktoenlarge.js
@@ -0,0 +1,130 @@
+// this script comes from here : https://www.thatsoftwaredude.com/content/6385/click-to-enlarge-script-with-pure-javascript
+// I modified it, but mostly for styling rather than function
+
+// TODO :
+// - centrer les images
+// ensuite moins urgent :
+// - assombrir le background quand l'overlay est ouvert
+
+
+var rand = Math.floor(Math.random() * 1000);
+// keeps collisions at bay
+var maxWidth = 0;
+var maxHeight = 0;
+var newWidth = 0;
+var newHeight = 0;
+
+// offset between window and image
+// used for centering
+var offsetx = 0;
+var offsety = 0;
+
+function init131() {
+    var images = document.querySelectorAll(".clicktoenlarge");
+    var body = document.querySelector("body");
+    var body_rect = body.getBoundingClientRect();		// used to calculate the maximum allowable (90%)
+
+    maxWidth = Math.floor(window.innerWidth * .9);		// maximum allowable width based on screen size
+    maxHeight = Math.floor(window.innerHeight * .9);		// maximum allowable height based on screen size
+
+    for (var i = 0 ; i < images.length; i++) {
+        var image = images[i];
+        var rect = image.getBoundingClientRect();
+        var hover_text = document.createElement("div");
+
+        hover_text.style.textAlign = "center";
+        hover_text.style.width = rect.width + "px";
+        image.style.transition = ".8s opacity";
+        image.style.cursor = "pointer";
+        image.addEventListener('click', function () {
+            var div = document.createElement("div");		// max allowable parent
+            var overlay = document.createElement("div");
+            var close = document.createElement("div");
+            var img = document.createElement("img");
+
+            // this styles the border that shows up when the image is enlarged, the overlay and the X button
+            div.style.cssText = "position:fixed;z-index:2000;border:ridge 4px #0fc900;border-radius:0px;overflow:hidden;background:none;text-align:center;";
+            // changed it for background: none
+            overlay.style.cssText = "background:none;opacity:.8;position:fixed;top:0px;width:100%;height:1000px;z-index:1000;left:0px;";
+            close.style.cssText = "background:#0fc900;color:black;cursor:pointer;position:absolute;top:0px;right:0px;font-size:20pt;width:30px;height:30px;border-radius:50%;text-align:center;";
+
+            overlay.className = "overlay_" + rand;
+            div.className = "clicktoenlarge_" + rand;
+
+            // only do this if the image size is larger than the max allowable
+            img.src = this.src;
+
+            // image is larger than the allowable space of 90%
+            if (this.naturalWidth > maxWidth || this.naturalHeight > maxHeight) {
+                newWidth = maxWidth;
+                newHeight = maxHeight;
+
+                // original version :
+                // div.style.width = maxWidth + "px";
+                //I removed '+ "px" ' and it fixed the problem of the image stretching. now the image keeps its original ratio
+                div.style.width = maxWidth;
+                div.style.height = maxHeight + "px";
+            }
+
+            else {
+                newWidth = this.naturalWidth;
+                newHeight = this.naturalHeight;
+
+                div.style.width = this.naturalWidth + "px";
+                div.style.height = this.NaturalHeight;
+                //also removed + px here
+            }
+
+            img.style.maxHeight = div.style.height;
+            //original version:
+            //div.style.width = img.style.width + "px";
+            //I removed '+ "px" ' and it fixed the problem of the image stretching. now the image keeps its original ratio. It does not adapt well when screen height is very low, but that will do for now, since people rarely have a screen that is very wide but very low.
+            div.style.width = img.style.width;
+
+            offsetx = Math.floor((window.innerWidth - newWidth) / 2);
+            //offsetx = Math.floor(window.innerWidth - newWidth); essayé d'enlever le /2 : c'est un peu moins pire mais pas réglé et ça bug sur mobile.
+            offsety = Math.floor((window.innerHeight - newHeight) / 2);
+
+            div.style.top = offsety + "px";
+            div.style.left = offsetx + "px";
+
+
+            close.innerHTML = "x";
+            close.addEventListener('click', function () {
+                var overlay = document.querySelector(".overlay_" + rand);
+                var clicktoenlarge = document.querySelector(".clicktoenlarge_" + rand);
+                body.removeChild(overlay);
+                body.removeChild(clicktoenlarge);
+            });
+
+
+            div.appendChild(close);
+            div.appendChild(img);
+            body.appendChild(div);
+            body.appendChild(overlay);
+
+        });
+        image.addEventListener('mouseover', function () {
+            // this.style.opacity = ".7";   // removed this so that the opacity doesnt change on hover
+            this.style.boxSizing = "border-box";
+        });
+        image.addEventListener('mouseout', function () {
+            this.style.opacity = "1";
+        });
+
+        if (image.nextSibling) {
+            image.parentNode.insertBefore(hover_text, image.nextSibling);
+        }
+        else {
+            image.parentNode.appendChild(hover_text);
+        }
+    }
+}
+
+//the if prevents it from running on mobile or very narrow screens, as it is not useful to do so.
+//it is not perfect : if you resize your browser window on desktop, you need to refresh the page for it to apply. But it is good enough for now.
+if (document.documentElement.clientWidth > 600 ) {
+  window.addEventListener('load', function () {
+      init131();
+  });
+}
